2c1e0a7483f7c2f842c8c8dff99524a93c3e347a
unknown <you@example.com>
2020-10-06 15:15:14 +0200

    Menu Application manager #4

--- src/app/navbar/navbar/navbar.component.ts
+++ src/app/navbar/navbar/navbar.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
+import { Collegue } from 'src/app/auth/auth.domains';
+import { AuthService } from 'src/app/auth/auth.service';
+
+
 
 @Component({
   selector: 'app-navbar',
@@ -9,9 +14,15 @@ import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 })
 export class NavbarComponent implements OnInit {
 
+ collegueConnecte: Observable<Collegue>
+
   links = [
-    { title: 'Home', fragment: 'accueil' },
-    { title: 'Connexion', fragment: 'connection' },
+    { title: 'Accueil', fragment: 'accueil' },
+    { title: 'Gestion des absences', fragment: 'gestion' },
+    { title: 'Planning des absences', fragment: 'planning' },
+    { title: 'Validation demandes', fragment: 'validation' },
+    { title: 'Vue synthétique', fragment: 'vue' },
+    { title: 'Jours feriés', fragment: 'jours' },
   ];
 
   faUserCircle = faUserCircle;
@@ -20,10 +31,11 @@ export class NavbarComponent implements OnInit {
   
   }
  
-
-  constructor(public route: ActivatedRoute) { }
+  constructor(public route: ActivatedRoute,
+    private authSrv : AuthService ) { }
 
   ngOnInit(): void {
+    this.collegueConnecte = this.authSrv.collegueConnecteObs;
   }
 
 }
