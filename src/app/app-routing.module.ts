0c36c2c6c9c0c4c9c28f0963c01dc0ce41b98afe
Teixeira <teixeirac.jordan@gmail.com>
2020-10-09 16:34:31 +0200

    route calendrier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { VisuAbsencesComponent } from './visu-absences/visu-absences.component';
 import { PageUtilisateurComponent } from './pages/page-utilisateur/page-utilisateur.component';
 import { PageAdministrateurComponent } from './pages/page-administrateur/page-administrateur.component';
 import { PageManagerComponent } from './pages/page-manager/page-manager.component';
+import { CalendrierAbsencesComponent } from './calendrier-absences/calendrier-absences/calendrier-absences.component';
 
 const routes: Routes =  [
   { path: 'tech', component: TechComponent, canActivate: [StatutConnecteService]}, // /tech accessible uniquement si connecté
@@ -16,6 +17,7 @@ const routes: Routes =  [
   { path: 'PageUtilisateurComponent/gestion', component: GestionAbsenceComponent},
   {path: 'PageManagerComponent/gestion', component: GestionAbsenceComponent},
   {path: 'PageAdministrateurComponent/gestion', component: GestionAbsenceComponent},
+  {path: 'PageAdministrateurComponent/planning', component: CalendrierAbsencesComponent},
   { path: 'PageUtilisateurComponent',  component:  PageUtilisateurComponent},
   { path: 'PageManagerComponent',  component: PageManagerComponent},
   { path: 'PageAdministrateurComponent', component: PageAdministrateurComponent},
