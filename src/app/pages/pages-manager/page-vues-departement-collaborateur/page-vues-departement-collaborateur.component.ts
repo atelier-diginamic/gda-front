c31752871bd31c5c929d75521bf24f3852b4e57a
Nicolas Tarbouriech <65289313+NicolasTarbouriech@users.noreply.github.com>
2020-10-14 17:56:28 +0200

    ajout page vue par jour par departement pr le manager

--- src/app/pages/pages-manager/page-vues-departement-collaborateur/page-vues-departement-collaborateur.component.ts
+++ src/app/pages/pages-manager/page-vues-departement-collaborateur/page-vues-departement-collaborateur.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbsenceService } from 'src/app/services/absence.service';
+import { Router } from '@angular/router';
+import { Absence } from 'src/app/entities/absence.model';
 
 @Component({
   selector: 'app-page-vues-departement-collaborateur',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageVuesDepartementCollaborateurComponent implements OnInit {
 
-  constructor() { }
+  constructor(private absenceService : AbsenceService, private router : Router) { }
+  erreurTechnique: boolean;
+  listeAllAbsences: Absence[];
+  listeAllAbsencesValider : Absence[];
+  aucuneAbsenceTrouvee: boolean;
 
   ngOnInit(): void {
+    this.afficherListeValidationDemandeAccepte();
   }
 
+  afficherListeValidationDemandeAccepte() {
+    this.listeAllAbsences = [];
+    this.listeAllAbsencesValider = [];
+    this.absenceService.listeAbsenceValider()
+    .subscribe(listeFromBack => {
+
+      this.erreurTechnique = false;
+      if (listeFromBack.length > 0) {
+        this.aucuneAbsenceTrouvee = false;
+        this.listeAllAbsences = listeFromBack;
+      } else {
+        this.aucuneAbsenceTrouvee = true;   
+      }
+      for (var absence of this.listeAllAbsences) {
+        //console.log(absence);
+        this.listeAllAbsencesValider.push(absence);
+      }
+    },
+      error => {
+        this.erreurTechnique = true;
+        console.log(error);
+      });
+  }
+
+  
 }
+  
+
---
+++ src/app/pages/pages-manager/page-vues-departement-collaborateur/selectbar/selectbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selectbar',
+  templateUrl: './selectbar.component.html',
+  styleUrls: ['./selectbar.component.scss']
+})
+export class SelectbarComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+}
