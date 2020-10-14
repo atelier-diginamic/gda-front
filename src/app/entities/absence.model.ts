5a1b4b0e88d3fd58779f46513f7788f59af343de
Nicolas Tarbouriech <65289313+NicolasTarbouriech@users.noreply.github.com>
2020-10-14 14:59:19 +0200

    non

--- src/app/entities/absence.model.ts
+++ src/app/entities/absence.model.ts
@@ -6,11 +6,10 @@ export class Absence {
         public typeConge: string;
         public commentaireAbsence: string;
         public statutDemande: string;
-
         public jourSemaine: String; 
-
         public idAbsence: number;
-
+        public nomDemandeur: string;
+        public prenomDemandeur: string;
         public toString(){
                 return `${this.idCollegue}  ${this.datePremierJourAbsence} ${this.dateDernierJourAbsence} ${this.typeConge} ${this.commentaireAbsence} ${this.statutDemande} `
         }
